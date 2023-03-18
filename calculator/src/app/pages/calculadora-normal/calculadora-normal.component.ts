dc5ce914585160b1344134bfbd0549f17516fae6
Rodolfo <rodolforomero.dev@gmail.com>
2023-03-18 11:41:12 -0300

    Edición operaciones DEL y cerrando algunas cosas pendientes

--- calculator/src/app/pages/calculadora-normal/calculadora-normal.component.ts
+++ calculator/src/app/pages/calculadora-normal/calculadora-normal.component.ts
@@ -52,7 +52,6 @@ export class CalculadoraNormalComponent {
         case 2:
           this.second_number = ((parseInt(this.second_number)) * (-1)).toString();
           this.result = this.second_number;
-          this.operation = '';
           break;
       }
     }
@@ -66,7 +65,9 @@ export class CalculadoraNormalComponent {
         this.actual_position = 2;
         break;
       case 2:
-        this.resolve()
+        if(this.first_number == '') {
+          this.first_number = '0';
+          this.resolve()
             .then( (res) => {
               this.result = res;
               this.actual_position = 2;
@@ -75,6 +76,17 @@ export class CalculadoraNormalComponent {
               this.second_number = '';
             })
             .catch( (error) => console.log(error));
+        } else if(this.second_number != '') {
+          this.resolve()
+            .then( (res) => {
+              this.result = res;
+              this.actual_position = 2;
+              this.first_number = res;
+              this.operation = oper;
+              this.second_number = '';
+            })
+            .catch( (error) => console.log(error));
+        }
         break;
     }
   }
@@ -99,18 +111,49 @@ export class CalculadoraNormalComponent {
     }
   }
 
+  //Comprende CE, C, <-
   setDeleteOperation(oper: string) {
-    switch (this.actual_position) {
-      case 1:
-        this.operation = oper;
-        this.actual_position = 2;
+    switch (oper) {
+      case 'c':
+        this.actual_position = 1;
+        this.first_number = '';
+        this.second_number = '';
+        this.operation = '';
+        this.result = '0';
         break;
-      case 2:
-        
+      case 'ce':
+        this.actual_position = 1;
+        this.first_number = '';
+        this.second_number = '';
+        this.operation = '';
+        this.result = '0';
+        break;
+      case 'del':
+        let val = '';
+        if(this.actual_position == 1) {
+          val = this.first_number.substring(0, this.first_number.length - 1);
+          if(val.length == 0 || val == '-') {
+            this.first_number = '';
+            this.result = '0';
+          } else {
+            this.first_number = val;
+            this.result = val;
+          }
+        } else if(this.actual_position == 2) {
+          val = this.second_number.substring(0, this.second_number.length - 1);
+          if(val.length == 0 || val == '-') {
+            this.second_number = '';
+            this.result = '';
+          } else {
+            this.second_number = val;
+            this.result = val;
+          }
+        }
         break;
     }
   }
 
+  //Función que se activa cuando se oprime "="
   getResult() {
     if((this.first_number != '') && (this.second_number != '')) {
       this.resolve()
@@ -132,6 +175,7 @@ export class CalculadoraNormalComponent {
     }
   }
 
+  //Función que realiza el cálculo
   async resolve(): Promise<any> {
     let res = 0;
     switch (this.operation) {
